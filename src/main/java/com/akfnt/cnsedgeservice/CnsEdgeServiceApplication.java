d4d58dfc1b876c25c0586372f2e375034443564f
hjPark <hj.park@navercorp.com>
2024-04-10 23:41:21 +0900

    init project

---
+++ src/main/java/com/akfnt/cnsedgeservice/CnsEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.akfnt.cnsedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsEdgeServiceApplication.class, args);
+	}
+
+}
