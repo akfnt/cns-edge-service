7b8ae311b0b06c7dde81886a233c73c665702511
akfnt <akfnt@naver.com>
2024-04-17 14:30:41 +0900

    update k8s deployment.yml (add polar-ui, keycloak env) and fix CnsEdgeServiceApplicationTests

--- src/test/java/com/akfnt/cnsedgeservice/CnsEdgeServiceApplicationTests.java
+++ src/test/java/com/akfnt/cnsedgeservice/CnsEdgeServiceApplicationTests.java
@@ -2,6 +2,8 @@ package com.akfnt.cnsedgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
@@ -15,6 +17,9 @@ import org.testcontainers.utility.DockerImageName;
 @Testcontainers
 class CnsEdgeServiceApplicationTests {
 	private static final int REDIS_PORT = 6379;
+
+	@MockBean
+	ReactiveClientRegistrationRepository clientRegistrationRepository;
 
 	@Container
 	static GenericContainer<?> redis = new GenericContainer<>((DockerImageName.parse("redis:7.0")))
