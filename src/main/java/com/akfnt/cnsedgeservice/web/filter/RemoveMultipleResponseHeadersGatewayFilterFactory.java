e6eadd858cae0bb6d9d09fffc304e1d855723bbf
akfnt <akfnt@naver.com>
2024-04-21 00:20:50 +0900

    add lombok and remove unnecessary import

--- src/main/java/com/akfnt/cnsedgeservice/web/filter/RemoveMultipleResponseHeadersGatewayFilterFactory.java
+++ src/main/java/com/akfnt/cnsedgeservice/web/filter/RemoveMultipleResponseHeadersGatewayFilterFactory.java
@@ -1,7 +1,6 @@
 package com.akfnt.cnsedgeservice.web.filter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -14,10 +13,9 @@ import java.util.List;
 // RequestRateLimiter 필터를 사용하면 Rate Limit 설정 값과 시간 윈도 내에 허용된 남은 요청 수 정보를 응답 헤더에 전송하게 되는데
 // 이 정보들은 악용될 소지가 있기 때문에 응답 헤더에서 제거하기 위한 커스텀 필터를 정의
 // Spring Cloud Gateway 에서 제공하는 RemoveResponseHeaderGatewayFilterFactory 도 있으나 정상적으로 동작하지 않아 새로 정의함
+@Slf4j
 @Component
 public class RemoveMultipleResponseHeadersGatewayFilterFactory extends AbstractGatewayFilterFactory<RemoveMultipleResponseHeadersGatewayFilterFactory.Config> {
-
-    private static final Logger logger = LoggerFactory.getLogger(RemoveMultipleResponseHeadersGatewayFilterFactory.class);
     public RemoveMultipleResponseHeadersGatewayFilterFactory() {
         super(Config.class);
     }
@@ -29,7 +27,7 @@ public class RemoveMultipleResponseHeadersGatewayFilterFactory extends AbstractG
                 List<String> headersToRemove = config.getHeadersToRemove();
                 if(headersToRemove != null && !headersToRemove.isEmpty()) {
                     for(String header : headersToRemove) {
-                        logger.debug("[RequestRateLimiter Info] header name: {}, header values: {}", header, response.getHeaders().get(header));
+                        log.debug("[RequestRateLimiter Info] header name: {}, header values: {}", header, response.getHeaders().get(header));
 
                         response.getHeaders().remove(header);
                     }
